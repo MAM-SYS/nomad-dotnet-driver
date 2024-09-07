143581a74bf9b9157169396425f4f4f10e2a8b97
mamsi <m.hossein95th@gmail.com>
2024-09-07 19:53:36 +0330

    test: add test-resources project

---
+++ test-resources/NancyWebApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+using System.IO;
+using System.Runtime;
+
+namespace NancyWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = new Uri("http://localhost:1234");
+            var hostConfig = new HostConfiguration { UrlReservations = new UrlReservations { CreateAutomatically = true } };
+
+            using (var host = new NancyHost(hostConfig, uri))
+            {
+                host.Start();
+                Console.WriteLine("NancyFX service running on " + uri);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class MyModule : NancyModule
+    {
+        public MyModule()
+        {
+            Get("/__health", _ =>
+            {
+                var response = new
+                {
+                    status = "ok",
+                    datetime = DateTime.UtcNow
+                };
+                return Response.AsJson(response);
+            });
+
+            Get("/config/gc", _ =>
+            {
+                var gcConfig = new
+                {
+                    ServerGC = GCSettings.IsServerGC,
+                    LatencyMode = GCSettings.LatencyMode.ToString()
+                };
+                return Response.AsJson(gcConfig);
+            });
+        }
+    }
+}
